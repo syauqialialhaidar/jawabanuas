2cd08cb8e2f908cf0c377952f262abe1a892da41
syauqialialhaidar <syauqialialhaidar@gmail.com>
2023-01-17 09:20:18 +0700

    oke 6

---
+++ src/UAS1B.java
@@ -0,0 +1,22 @@
+public class UAS1B {
+    private String userID;
+    private String userName;
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public
+
+}
